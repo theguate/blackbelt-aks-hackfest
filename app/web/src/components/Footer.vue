aabbbe9fe99c899f1f28a5b3bfdb49b99406c122
theguate <galvezjavier@hotmail.com>
2018-07-12 12:10:12 -0700

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Javier Team
+        Javier 2 Team
       </div>
       <div class="col-lg-6">
       </div>
