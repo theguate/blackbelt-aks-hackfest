15c5ec0f65d508936400528e7fb6e6965e4e6a2a
theguate <galvezjavier@hotmail.com>
2018-07-12 11:56:58 -0700

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Javier Team
       </div>
       <div class="col-lg-6">
       </div>
